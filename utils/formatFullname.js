166084f8fb348a4872bf04e26655dd414800a623
Marcin Skowronek <marcinfoto35@op.pl>
2021-10-30 00:07:48 +0200

    Pass tests by function

--- utils/formatFullname.js
+++ utils/formatFullname.js
@@ -1,6 +1,8 @@
 module.exports = (fullName) => {
   if (typeof fullName !== 'string') return 'Error';
-  const [firstName, lastName] = fullName.split(' ');
-  if (!firstName || !lastName) return false;
+  const [firstName, lastName, notAllowed] = fullName.split(' ');
+  if(notAllowed) return 'Error';
+  if (!firstName || !lastName) return 'Error';
+  if(fullName.includes('.')||fullName.includes(',') || fullName.includes(';')) return 'Error';
   return firstName[0].toUpperCase() + (firstName.slice(1).toLowerCase()) + ' ' + lastName[0].toUpperCase() + lastName.slice(1).toLowerCase();
 };
--- utils/test/formatFullname.test.js
+++ utils/test/formatFullname.test.js
@@ -12,6 +12,18 @@ describe('formatFullname', () => {
   });
 
   it('should return an error if "content" has uncorrect format (other than <firstname><lastname>)', () => {
-    expect(formatFullname('john doe')).to.equal('John Doe');
+    expect(formatFullname(null)).to.equal('Error');
+    expect(formatFullname('john ')).to.equal('Error');
+    expect(formatFullname(' doe')).to.equal('Error');
+    expect(formatFullname('john doe test')).to.equal('Error');
+    expect(formatFullname('john doe test test')).to.equal('Error');
+    expect(formatFullname('; doe')).to.equal('Error');
+    expect(formatFullname('john .')).to.equal('Error');
+  });
+
+  it('should return a correct "content" (ex. John Doe) if "content" has uncorrect upper and lower case (ex. JoHn doE', () => {
+    expect(formatFullname('joHn DoE')).to.equal('John Doe');
+    expect(formatFullname('JOHN DOE')).to.equal('John Doe');
+    expect(formatFullname('jOHN dOE')).to.equal('John Doe');
   });
 });
